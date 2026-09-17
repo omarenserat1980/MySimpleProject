f7540c1f29670ca9de8693dcb8d1e29748bd9fd8
omarenserat1980 <154518514+omarenserat1980@users.noreply.github.com>
2026-09-17 20:11:34 +0300

    Finalize Android module

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,13 +1,2 @@
 plugins { id("com.android.application") }
-
-android {
-    namespace = "com.foras.ads"
-    compileSdk = 35
-    defaultConfig {
-        applicationId = "com.foras.ads"
-        minSdk = 23
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-}
+android { namespace = "com.foras.ads"; compileSdk = 35; defaultConfig { applicationId = "com.foras.ads"; minSdk = 23; targetSdk = 35; versionCode = 1; versionName = "1.0" } }
