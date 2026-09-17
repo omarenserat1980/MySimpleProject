c5910debd45afe7bce6e611d79cd26735e19b7e7
omarenserat1980 <154518514+omarenserat1980@users.noreply.github.com>
2026-09-17 20:09:53 +0300

    Add Android WebView app shell for Foras Ads

---
+++ android/app/src/main/java/com/foras/ads/MainActivity.java
@@ -0,0 +1,25 @@
+package com.foras.ads;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private static final String SERVER_URL = "http://127.0.0.1:8080/";
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView web = new WebView(this);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        web.loadUrl(SERVER_URL);
+        setContentView(web);
+        Toast.makeText(this, "Foras Ads", Toast.LENGTH_SHORT).show();
+    }
+}
